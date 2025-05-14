b79133f76c0d741ce9f2c671550f02d9e0541a9e
Lutrino <lutrinotech@gmail.com>
2025-05-14 10:47:53 -0500

    [TASK-001] creating a layout, definying a basic theme, and installing a icon library

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="text-3xl font-bold text-center underline flex items-center justify-center h-screen">
+      <h1>Home page !</h1>
+    </div>
+  );
+};
+
+export default HomePage;
