26b0f16919e516554aa2e01106cc44168bffd9b4
Lutrino <lutrinotech@gmail.com>
2025-05-15 22:32:19 -0500

    [TASK-001] Creating a static version of the home page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,8 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const MainLayout = ({ children }) => {
   return (
@@ -8,12 +10,15 @@ const MainLayout = ({ children }) => {
       <header>
         <Container fluid className="header-container">
           <Row className="header-container_row">
-            <Col className="header-container_column" xs={12} md={9}>
+            <Col className="header-container_column" xs={10} md={11}>
               <h6>Relatos de papel</h6>
             </Col>
 
-            <Col className="header-container_column" xs={12} md={3}>
-              <p>Cart</p>
+            <Col className="header-container_column" xs={2} md={1}>
+              <div className="header-container_icons">
+                <FontAwesomeIcon icon={faCartShopping} size="lg" color="gray" />
+                <FontAwesomeIcon icon={faUser} size="lg" color="gray" />
+              </div>
             </Col>
           </Row>
         </Container>
