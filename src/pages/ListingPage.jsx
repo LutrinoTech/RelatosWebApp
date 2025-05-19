03f356008e1720b663a9dad367c526fb0031bd7b
Lutrino <lutrinotech@gmail.com>
2025-05-18 20:03:11 -0500

    [TASK-006] Adding search feature (#13)

--- src/pages/ListingPage.jsx
+++ src/pages/ListingPage.jsx
@@ -2,20 +2,34 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
-import { books } from "../utils/data";
+import { useGetBooks } from "../hooks/useGetBooks";
 import CustomCard from "../components/CustomCard";
 
 const ListingPage = () => {
+  const { books, title, setTitle } = useGetBooks();
+
   const allBooks = books.map((book) => (
     <CustomCard key={book.id} book={book} />
   ));
+
+  const handleInputField = (event) => {
+    setTitle(event.target.value);
+    console.log(event.target.value);
+  };
 
   return (
     <Container>
       <Row>
         <Col>
-          <Form.Label htmlFor="bookTitle">Buscar</Form.Label>
-          <Form.Control type="text" id="bookTitle" />
+          <Form.Label htmlFor="title">Buscar</Form.Label>
+
+          <Form.Control
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            onChange={handleInputField}
+          />
         </Col>
       </Row>
 
