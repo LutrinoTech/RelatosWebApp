0b5aedb490080bb667d5785901d3d5aa13e279fd
Lutrino <lutrinotech@gmail.com>
2025-07-04 00:39:31 -0500

    [TASK-012] Fixing the issue on the cart when item were deleted

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -3,7 +3,12 @@ import { create } from "zustand";
 const useStore = create((set) => ({
   cartItems: [],
   addToCart: (item) =>
-    set((state) => ({ cartItems: [...state.cartItems, item] })),
+    set((state) => {
+      if (state.cartItems.some((cartItem) => cartItem.id === item.id)) {
+        return {};
+      }
+      return { cartItems: [...state.cartItems, item] };
+    }),
   removeFromCart: (id) =>
     set((state) => ({
       cartItems: state.cartItems.filter((item) => item.id !== id),
